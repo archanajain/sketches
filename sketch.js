d22c15dd0330112a1b39e8df99bfb65c808ac0bc
archanajain <archana0808@gmail.com>
2016-04-24 14:00:58 -0700

    Added files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(800,800);
+}
+
+function draw() {
+  
+  background(255,255,255);
+  
+  fill(255,0,0);
+  ellipse(mouseX,mouseY,50,50);
+  
+  fill(0,0,255);
+  text("Archana",mouseX, mouseY);
+  
+}
+
